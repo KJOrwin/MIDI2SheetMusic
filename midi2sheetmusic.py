8f461ba868c7d769b7daf691149ae15338ec9dfc
KJOrwin <18orwink@clevedonschool.org.uk>
2024-12-01 21:07:34 +0000

    Added input sanitation.

--- midi2sheetmusic.py
+++ midi2sheetmusic.py
@@ -1,6 +1,20 @@
+#Import external libraries
+import os
+
+#Reads an inputted midi file
 def import_MIDI(filename):
+    #Input sanitation
+    if len(filename) == 0:
+        return "Error: No file was entered"
+    elif not os.path.exists(os.path.abspath(filename)):
+        return "Error: File entered does not exist"
+    elif not filename.endswith(".mid"):
+        return "Error: File entered is not a midi file"
+    #Read inputted midi file
     with open(filename, "rb") as file:
         return file.read()
 
-input_file = input("Enter the midi file name: ")
-import_MIDI(input_file)
+if __name__ == "__main__":
+    #Store inputted midi file
+    input_file = input("Enter the midi file name: ")
+    midifile = import_MIDI(input_file)
