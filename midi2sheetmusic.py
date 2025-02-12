b1558c943d60d167b7550ea564e2888564de5a11
KJOrwin <18orwink@clevedonschool.org.uk>
2025-02-12 22:44:49 +0000

    Added a complete midi file fr testing and attempted to allow for multi-track midifiles. Found a very serious bug where most midi files will only have one 'note on' command for the entire track.

--- midi2sheetmusic.py
+++ midi2sheetmusic.py
@@ -33,9 +33,10 @@ class MIDI_Metadata:
     def getTimeDivision(self):
         return self.time_division
 
-class Track_Metadata:
-    def __init__(self, track_header: bytes, track_header_length: bytes):
+class Track:
+    def __init__(self, track_header: bytes, track_header_length: bytes, events: list):
         self.track_header = self.track_header_length = None
+        self.events = events
         if len(track_header) == 4:
             self.track_header = track_header
         if len(track_header_length) == 4:
@@ -48,6 +49,9 @@ class Track_Metadata:
 
     def getTrackHeaderLength(self):
         return self.track_header_length
+
+    def getEvents(self):
+        return self.events
     
 class Event:
     def __init__(self, delta_time: bytes, event: bytes, data: bytes):
@@ -86,43 +90,64 @@ if __name__ == "__main__":
     input_file = input("Enter the midi file name: ")
     midifile = import_MIDI(input_file)
     mmetadata = MIDI_Metadata(midifile[0:4], midifile[4:8], midifile[8:10], midifile[10:12], midifile[12:14])
-    tmetadata = Track_Metadata(midifile[14:18], midifile[18:22])
-    event_list = []
-    startpos = 22
-    endpos = 23
-    while True:
-        #Calulating deltatime
-        #If the first bit of the final byte is zero then it is the final byte in deltatime, if not continue
+    startpos = 14
+    endpos = 15
+    tracks = []
+    for i in range(int.from_bytes(mmetadata.getNumTracks())):
+        temp_track_header = midifile[startpos:startpos+4]
+        temp_track_header_length = midifile[startpos+4:startpos+8]
+        startpos += 8
+        endpos += 8
+        temp_events = []
         while True:
-            if format(midifile[endpos - 1], "08b").startswith("0"):
-                temp_deltatime = midifile[startpos:endpos]
+            #Calulating deltatime
+            #If the first bit of the final byte is zero then it is the final byte in deltatime, if not continue
+            while True:
+                if format(midifile[endpos - 1], "08b").startswith("0"):
+                    temp_deltatime = midifile[startpos:endpos]
+                    break
+                elif format(midifile[endpos - 1], "08b").startswith("1"):
+                    endpos += 1
+            startpos = endpos
+            endpos += 1
+            #Check if at the end of file
+            if bytes(midifile[startpos:startpos+3]) == b'\xff/\x00':
+                temp_events.append(Event(temp_deltatime, b'\xff/\x00', None))
+                startpos += 3
+                endpos += 3
                 break
-            elif format(midifile[endpos - 1], "08b").startswith("1"):
-                endpos += 1
-        startpos = endpos
-        endpos += 1
-        #Check if at the end of file
-        if bytes(midifile[startpos:startpos+3]) == b'\xff/\x00':
-            break
-        #Calculate length of event
-        #If the event starts with byte FF then the event will have data
-        if format(midifile[startpos], "08b") == "11111111":
-            temp_event = midifile[startpos:startpos+3]
-            startpos += 2
-            temp_data = midifile[startpos:startpos+midifile[startpos]]
-            event_list.append(Event(temp_deltatime, temp_event, temp_data))
-            startpos += midifile[startpos]
-        #If the event starts with byte Cn or Dn then the event will always be 2 bytes long
-        elif format(midifile[startpos], "08b").startswith("1100") or format(midifile[startpos], "08b").startswith("1101"):
-            event_list.append(Event(temp_deltatime, midifile[startpos:startpos+2], None))
-            startpos += 2
-        #Otherwise do the event will always be 3 bytes long
-        else:
-            event_list.append(Event(temp_deltatime, midifile[startpos:startpos+3], None))
-            startpos += 3
-        endpos = startpos + 1
-    print(event_list)
+            #Calculate length of event
+            #If the event starts with byte FF then the event will have data
+            # for e in ["1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111", "11111111"]:
+            #     if not format(midifile[startpos], "08b").startswith(e):
+            #         if e == "11111111":
+            #             print(temp_deltatime, midifile[startpos:startpos+8])
+            #             print("no")
+            if format(midifile[startpos], "08b") == "11111111":
+                temp_event = midifile[startpos:startpos+3]
+                startpos += 3
+                temp_data = midifile[startpos:startpos+midifile[startpos-1]]
+                temp_events.append(Event(temp_deltatime, temp_event, temp_data))
+                startpos += midifile[startpos-1]
+            #If the event starts with byte Cn or Dn then the event will always be 2 bytes long
+            elif format(midifile[startpos], "08b").startswith("1100") or format(midifile[startpos], "08b").startswith("1101"):
+                temp_events.append(Event(temp_deltatime, midifile[startpos:startpos+2], None))
+                startpos += 2
+            #Otherwise do the event will always be 3 bytes long
+            else:
+                temp_events.append(Event(temp_deltatime, midifile[startpos:startpos+3], None))
+                startpos += 3
+            endpos = startpos + 1
+
+            print(f"{temp_events[-1].getDeltaTime()}\t{temp_events[-1].getEvent()}\t{temp_events[-1].getData()}")
+
+        tracks.append(Track(temp_track_header, temp_track_header_length, temp_events))
+    
+    print(tracks)
+    for element in tracks:
+        print(element.getEvents())
     
 
-    # for element in event_list:
-    #     print(f"{list(element.getDeltaTime())}\t{list(element.getEvent())}")
+#TO DO
+#Change track metadata to track data and store the events assciated to the tracks in the class
+#Allow for multiple tracks
